6931cb36cb7a161a6558c46da9bc9575e82d4455
Gh0stR0ck <wesleyjansen88@hotmail.com>
2017-10-17 10:05:55 +0200

    First project

---
+++ src/main/java/com/capgemini/movieswatched/MovieswatchedApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.movieswatched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieswatchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieswatchedApplication.class, args);
+	}
+}
